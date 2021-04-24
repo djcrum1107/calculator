d6e19800aad610d229872d230066fdf38e94ebc1
djcrum1107 <djcrum1107@gmail.com>
2021-04-23 22:51:42 -0400

    Finished basic calculator math function

--- javascript.js
+++ javascript.js
@@ -7,17 +7,51 @@ let operandRe = /\/|\*|\-|\+/;
 let inputString = "";
 let operand = "";
 
+//All buttons pressed will add to the input string with the exception of backspace and clear
 function buttonPressed(e){
     let value = e.target.value;
+    //If the button is a number only add it to the string that is in display
     if(numericalRe.test(value)){
         inputString += `${value}`;
         display.innerText = inputString;
     };
     if(operandRe.test(value)){
+        //Ignore if an operand is called before any number do nothing
+        if(inputString.length == 0){
+            return;
+        }
+        if(typeof(inputString) != "string"){
+            inputString = inputString.toString();
+        };
+        //If a second operand is added the previous one is removed first
+        if(operandRe.test(inputString[inputString.length - 1])){
+            inputString = inputString.slice(0, -1)
+        }
+        //If there is already an operator but numbers on both sides equate before adding another operand
+        if(inputString.match(operandRe)){
+            equate()
+        }
+        //Operand is saved separately for equate use
         operand = value;
-        console.log(operand);
+        inputString += `${value}`;
+        display.innerText = inputString;
+    }
+    if (value == "="){
+        //If there is no operand no equation can be done
+        if(operand == ""){return}
+        if(typeof(inputString) == "number"){
+            inputString = inputString.toString();
+        }
+        if(operandRe.test(lastChar(inputString)) || lastChar(inputString) == "="){
+            inputString = inputString.slice(0, -1)
+        }
+        equate();
     };
 };
+
+function lastChar(inputString){
+    return inputString[inputString.length - 1]
+}
 
 
 // This is an initial function to dynamically add to the grid area based
@@ -29,19 +63,46 @@ function buttonStyleSetup(){
     });
 };
 
-function add(x, y){
+function equate(){
+    let [x, y] = inputString.split(operand);
+    if(isNaN(x)){x = null}else{x = Number(x)};
+    if(isNaN(y)){y = null}else{y = Number(y)};
+    let result = 0;
+    switch(operand){
+        case "+":
+            result = add(x, y);
+            break;
+        case "-":
+            result = subtract(x, y);
+            break;
+        case "*":
+            result = multiply(x, y);
+            break;
+        case "/":
+            result = divide(x, y);
+    }
+    if(typeof(result) != "string"){
+        inputString = result.toString();
+    }else{
+        inputString = result;
+    };
+    operand = "";
+    display.innerText = inputString;
+};
+
+function add(x, y = 0){
     return x + y;
 }
 
-function subtract(x, y){
+function subtract(x, y = 0){
     return x - y;
 }
 
-function multiply(x, y){
+function multiply(x, y = 1){
     return x * y;
 }
 
-function divide(x, y){
+function divide(x, y = 1){
     if(y == 0){
         display.innerText = "SNARKY ERROR";
         return 0;
